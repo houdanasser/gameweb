fb30b04bb0580bee430e4dcad5446295a6686fbc
houdanasser <48397231+houdanasser@users.noreply.github.com>
2019-03-10 00:12:11 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# gameweb
+# AwesomeTheSauce
+Fan webpage for AwesomeTheSauce wiztuber
+
+First, to start uploading files in repository from computer directly, have to create a readme file or something so github can take you to the page insider repository where you can upload the files (pull-request). the file can be deleted afterwards...but dont know why github does that mandatory step ...
+
+When uploading on heroku, heroku is case sensitive (lower case) for images! also, use png images!
+
+When deploying on heroku, make sure the PHP file is at the root of the repository, because heroku recognizes PHP not a folder as root.
+this means since the app is being deployed on heroku directly from github, github must have the index file right at the root of the repository
+NOT inside a folder!
+
+To test PHP file locally, run Apache, and make sure w3svc service is stopped because PHP needs to use port 80/443:
+"net stop w3svc"
+once Apache starts, you can restart it "net start w3svc"
+
+Also, every commit thats done here, if the heroku setting for automatic deployments is checked, this means every commit/edit here pushed/committed takes effects right away at the deployed webpage!
+meaning no access is necessary to my heroku account, this repository can be updated to reflect new things on the webpage right from here!
---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <title>GameWeb</title>
+      <!--<link rel="SHORTCUT ICON" HREF="images/logo.png">-->
+      <link href="css/bootstrap.min.css" rel="stylesheet">
+      <!-- Bootstrap CSS -->
+      <link href="css/homepageStyle.css" rel="stylesheet">
+      <script src="javascript/jquery.js"></script>
+      <script src="javascript/bootstrap.min.js"></script> <!-- Bootstrap JavaScript -->
+	<script src="https://apis.google.com/js/platform.js"></script> <!-- YoutTube -->
+   </head>
+   <div class="RetroIntroductionHeaderStyle">
+      <div class="container">
+         <div class="col-lg-12">
+            <!--col-lg, (large) the columns will stack when the width is < 1200px.-->
+            <div class="RetroIntroductionMessageStyle">
+               <h1 style="color:white;"><i>GameWeb by:Houda M.Nasser</i></h1>
+               <h3 style="color:lightgreen;">GameWeb forever ~<!--<br><i>~</i> Saucia.</br>--> </h3>
+               <hr class="RetroIntroductionDivider">
+            </div>
+         </div>
+      </div>
+   </div>
+   <div class="ContentSection2">
+      <div class="container">
+         <div class="col-lg-5 col-lg-offset-1 col-sm-push-6  col-sm-6">
+            <hr class="HeadingOfSection-space">
+            <div class="clearfix"></div>
+            <h2 class="HeadingOfSection">Who am I? Houda Nasser</h2>
+            <p class="writing">The coolest game website ever! DANG I SOUND REALLY FORMAL.</p>
+			<!--<button class="btn1" type="button" onclick="window.open ('https://www.wizard101.com/', ''); return false" href="javascript:void(0);">Twitch!</button>-->
+         </div>
+         <div class="col-lg-5 col-sm-pull-6  col-sm-6">
+            <img class="img-responsive" src="images/Thomas.png" alt="Thomas">
+         </div>
+      </div>
+   </div>
+   <div class="ContentSection1">
+      <div class="container">
+         <div class="col-lg-5 col-sm-6">
+            <hr class="HeadingOfSection-space">
+            <div class="clearfix"></div>
+            <h2 class="HeadingOfSection">What is yo Purr-Pose??</h2>
+            <p class="writing">WHAT UP PLAYAS? Welcome to Gameweb My goal is to create a website where people can be able to play a game without having to download it!</p>
+		<div class="g-ytsubscribe" data-channelid="UC2FscK2dqX3s_m4ESWCnppA" data-layout="full" data-count="default"></div>
+         </div>
+         <div class="col-lg-5 col-lg-offset-2 col-sm-6">
+           <!-- <iframe width="100%" height="250" src="https://www.youtube.com/embed/eu5pFpfQJA4" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>-->
+         </div>
+      </div>
+   </div>
+   <footer>
+      <div class="container">
+         <div class="col-lg-12">
+            <ul class="list-inline">
+               <li>
+                  <a href="#">Top of Page</a>
+               </li>
+            </ul>
+            <p class="copyright">Copyright &copy; GameWeb 2018. All Rights Reserved</p>
+		<p class="copyright">Webpage developed by:</p>
+		<div class="g-ytsubscribe" data-channelid="UC8hvrwd0StsX7TSdZ3O2bmg" data-layout="full" data-count="default"></div>
+         </div>
+      </div>
+   </footer>
+   </body>
+</html>
