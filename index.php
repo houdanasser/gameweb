206e741298317b8b3dfee3c38999f43bd01bf3c8
houdanasser <48397231+houdanasser@users.noreply.github.com>
2019-03-10 01:29:45 -0500

    Update index.php

--- index.php
+++ index.php
@@ -55,7 +55,7 @@
    </div>
    <div class="ContentSection2">
       <div class="container">
-	<div class="col-lg-5 col-lg-offset-1 col-sm-push-2  col-sm-4"><!-- center alignment by bootstrap; col-sm-push-1 seems to be the main factor for alignment here...-->
+	<div class="col-lg-5 col-lg-offset-1 col-sm-push-1  col-sm-4"><!-- center alignment by bootstrap; col-sm-push-1 seems to be the main factor for alignment here...-->
 	<embed width="725" height="835" src="https://mk.mgechev.com/"></embed><br/>
 	</div>
       </div>
